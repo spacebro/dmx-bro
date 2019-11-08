41a5b6aec6f26dc2eabaed82011c3f7acfcf11a1
hugohil <hilairehugo@gmail.com>
2019-11-08 17:31:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DMX-bro
+
+
+## Get device id
+
+```sh
+# Mac OS
+$ ioreg -p IOUSB -l -w 0
+
+# Linux
+$ lsusb
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const standardSettings = require('standard-settings')
+const settings = standardSettings.getSettings()
+
+const DMX = require('dmx')
+const { SpacebroClient } = require('spacebro-client')
+
+const client = new SpacebroClient({ ...settings.spacebro })
+
+const dmx = new DMX()
+
+for (const universe in settings.dmx.universes) {
+  console.log(universe)
+  dmx.addUniverse(
+    universe,
+    settings.dmx.universes[universe].output.driver,
+    settings.dmx.universes[universe].output.device,
+    settings.dmx.universes[universe].output.options
+  )
+}
+
+settings.events.forEach((event) => {
+  client.on(event.name, (datas) => {
+    const universe = datas.universe || event.universe || "default"
+    const values = datas.values || event.values || {}
+    const delay = datas.delay || event.delay || 0
+
+    console.log(`sending "${event.name}" to ${universe}`)
+    if (delay) console.log(`waiting ${delay}s...`)
+
+    setTimeout(() => {
+      dmx.update(universe, values)
+    }, (delay * 1000))
+  })
+})
